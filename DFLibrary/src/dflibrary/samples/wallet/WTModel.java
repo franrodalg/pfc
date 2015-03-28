121f97c223abb4f3d72e387f5493fb67c52b89f2
Francisco Rodríguez Algarra <francisco.rodriguez.algarra@gmail.com>
2015-03-28 17:18:03 +0100

    Changed type of variable cm to ComManager interface

--- DFLibrary/src/dflibrary/samples/wallet/WTModel.java
+++ DFLibrary/src/dflibrary/samples/wallet/WTModel.java
@@ -29,6 +29,7 @@ public class WTModel {
 		
 		this.view = view;
 		this.currApp = WTApp.SELECT_READER;
+		//this.cm = new SCIOComManager();
 		this.cm = new JPCSCComManager();
 		this.opt = ConfigOption.FORMAT_AND_CONFIG;
 		this.bdb = new BankDBManager(view);
@@ -1400,7 +1401,7 @@ public class WTModel {
 	
 	private WTView view;
 	private WTApp currApp;
-	private JPCSCComManager cm;
+	private ComManager cm;
 	
 	private BankDBManager bdb;
 	private TickDBManager tdb;
