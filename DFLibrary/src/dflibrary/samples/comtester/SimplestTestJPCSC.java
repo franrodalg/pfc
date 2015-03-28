8c78a8cf8cb5a25da574702338237186b33abe03
Francisco Rodríguez Algarra <francisco.rodriguez.algarra@gmail.com>
2015-03-28 17:20:10 +0100

    Reduced indentation and printing connected readers names

--- DFLibrary/src/dflibrary/samples/comtester/SimplestTestJPCSC.java
+++ DFLibrary/src/dflibrary/samples/comtester/SimplestTestJPCSC.java
@@ -10,25 +10,25 @@ public class SimplestTestJPCSC {
 		
 		ComManager cm = new SCIOComManager();		
 		try{
-				cm.scan();				
-				String[] readers = cm.listReaders();
-				System.out.println(Arrays.toString(readers));
-				cm.select(readers[0]);
-				
-				if(cm.isCardPresent()){					
-					cm.connect();					
-					CardType ct = cm.getCardType();			
-					if(ct == CardType.MIFARE_DESFIRE){										
-						DFCard df = new DFCard(cm);						
-						System.out.println(df.getKeyVersion(0));												
-					}										
-					cm.disconnect();									
-				}					
-				cm.deselect();				
-			}catch(DFLException e){	e.printStackTrace();}
-			try{
-				cm.release();
-			}
-			catch(DFLException e){e.printStackTrace();}
+			cm.scan();				
+			String[] readers = cm.listReaders();
+			System.out.println(Arrays.toString(readers));
+			cm.select(readers[0]);
+			
+			if(cm.isCardPresent()){					
+				cm.connect();					
+				CardType ct = cm.getCardType();			
+				if(ct == CardType.MIFARE_DESFIRE){										
+					DFCard df = new DFCard(cm);						
+					System.out.println(df.getKeyVersion(0));												
+				}										
+				cm.disconnect();									
+			}					
+			cm.deselect();				
+		}catch(DFLException e){	e.printStackTrace();}
+		try{
+			cm.release();
+		}
+		catch(DFLException e){e.printStackTrace();}
 	}	
 }
